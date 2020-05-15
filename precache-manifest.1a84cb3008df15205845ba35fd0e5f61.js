6addaad9a202919bc26cae79c55caf1b6f25813c
Vaibhav <vjainbhl@gmail.com>
2020-05-15 17:13:18 +0530

    Updates

---
+++ precache-manifest.1a84cb3008df15205845ba35fd0e5f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5246453dcb329d6ebe2622631d4cc791",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "5868e3d3e66cf33eddbe",
+    "url": "/Monster-rolodex/static/css/main.2d95c7a1.chunk.css"
+  },
+  {
+    "revision": "039ef357a225d874e979",
+    "url": "/Monster-rolodex/static/js/2.e1b681d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-rolodex/static/js/2.e1b681d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5868e3d3e66cf33eddbe",
+    "url": "/Monster-rolodex/static/js/main.b81745f5.chunk.js"
+  },
+  {
+    "revision": "eb5cbf8a1be2784f7007",
+    "url": "/Monster-rolodex/static/js/runtime-main.ea487236.js"
+  }
+]);
